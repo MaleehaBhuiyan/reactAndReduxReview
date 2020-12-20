8da4229e2eb2ef73e29b37dfa625e76c9e63d18e
MaleehaBhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-12-20 15:14:03 -0500

    React Basics Complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React from 'react'
 import Ninjas from './Ninjas'
+import AddNinja from './AddNinja'
 
 
 class App extends React.Component{
   state = {
     ninjas: [
       {name: "Maleeha", age: 21, belt: "Black", id: 1},
-      {name: "Lydia", age: 21, belt: "Yellow", id: 2},
+      {name: "Lydia", age: 20, belt: "Yellow", id: 2},
       {name: "Nicole", age: 21, belt: "Purple", id: 3}
     ]
   }
 
+  addNinja = (ninja) => {
+    ninja.id = Math.random()
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id 
+    })
+    this.setState({ ninjas: ninjas})
+  }
 
   render(){
     return(
       <>
       <h1>Welcome</h1>
-      <Ninjas ninjas={this.state.ninjas} />
+      <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas} />
+      <AddNinja addNinja={this.addNinja} />
       </>
     )
   }
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import './Ninjas.css'
 
 class Ninjas extends React.Component{
     render(){
-        const { ninjas } = this.props
+        const { ninjas, deleteNinja } = this.props
         const ninjaList = ninjas.map(ninja =>{
-            return (
-                <div className="ninja" key={ninja.id}>
-                <div>Name: {ninja.name}</div>
-                <div>Age: {ninja.age}</div>
-                <div>Belt: {ninja.belt}</div>
-                </div>
-            )
+            if (ninja.age > 20){
+                return (
+                    <div className="ninja" key={ninja.id}>
+                    <div>Name: {ninja.name}</div>
+                    <div>Age: {ninja.age}</div>
+                    <div>Belt: {ninja.belt}</div>
+                    <button onClick={() => {deleteNinja(ninja.id)}}>Delete Ninja</button>
+                    </div>
+                ) 
+            } else {
+                return null
+            }
         })
         return(
             ninjaList
